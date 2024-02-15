67050f2fbd2f14a9194593091504393d3b6035d0
Oliwier <worekit@gmail.com>
2024-02-15 17:57:12 +0100

    words input

--- src/Wordle.jsx
+++ src/Wordle.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
 import { LetterButton } from './components/LetterButton.jsx'
+import { LetterTile } from './components/LetterTile.jsx'
 import "./style.scss";
+
+const words = ["", ""];
+const currentWord = 1;
+
+const WordsColumns = [1, 2, 3, 4, 5]
+const WordsRows = [1, 2, 3, 4, 5, 6]
 
 const FirstRowLetters = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"]
 const SecondRowLetters = ["A", "S", "D", "F", "G", "H", "J", "K", "L"]
 const ThirdRowLetters = ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "BACKSPACE"]
 
 export function Wordle() {
-
 
   return (
     <>
@@ -17,21 +22,25 @@ export function Wordle() {
         </section>
 
         <section id="game" className="wordle-game-words">
-          <div>Game</div>
+          {WordsRows.map((row) => (
+            <div key={row} className={`wordle-tile-row`}>
+              { WordsColumns.map((index, col) => (<LetterTile key={index} letter={col}/>))}
+            </div>
+          ))}
         </section>
 
         <section id="keyboard" className="wordle-keyboard">
-          <div className="wordle-keyboard-row">
+          <div className="wordle-keyboard-row-first">
             {FirstRowLetters.map((letter) => (
-              <LetterButton key={letter} letter={letter} />
+              <LetterButton key={letter} letter={letter}/>
             ))}
           </div>
-          <div className="wordle-keyboard-second-row">
+          <div className="wordle-keyboard-row-second">
             {SecondRowLetters.map((letter) => (
               <LetterButton key={letter} letter={letter} />
             ))}
           </div>
-          <div className="wordle-keyboard-row">
+          <div className="wordle-keyboard-row-third">
             {ThirdRowLetters.map((letter) => (
               <LetterButton key={letter} letter={letter} />
             ))}
---
+++ src/components/LetterTile.jsx
@@ -0,0 +1,10 @@
+
+
+export function LetterTile({ letter }) {
+    return (
+      <>
+          <div className={`wordle-tile`}></div>
+      </>
+    )
+  }
+  
